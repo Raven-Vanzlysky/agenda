49d74dd2ad5d6c8b745fb7713a404a108f352bd5
Ioreum <mandalaraven@proton.me>
2024-10-02 11:07:11 +0700

    menambahkan teble-responsive

--- ^admin/admin_akun.php
+++ ^admin/admin_akun.php
@@ -117,57 +117,62 @@
           Tambah
         </button>  
 
-        <table id="table" class="table table-responsive table-bordered table-hover">
-          <thead class="text-center">
-            <tr>
-              <th class="bg-secondary">No</th>
-              <th class="bg-secondary">NIP</th>
-              <th class="bg-secondary">Nama</th>
-              <th class="bg-secondary">Jenis Kelamin</th>
-              <th class="bg-secondary">Agama</th>
-              <th class="bg-secondary">Foto</th>
-              <th class="bg-secondary">Email</th>
-              <th class="bg-secondary">Username</th>
-              <th class="bg-secondary">Level</th>
-              <th class="bg-secondary">Opsi</th>
-            </tr>
-          </thead>
+        <div class="table-responsive">
 
-          <tbody>
-            <?php $no = 1; ?>
-              <?php foreach ($data_guru as $x) : ?>
-                <tr style="height: 15px;">
-                  <td><?= $no++; ?></td>
-                  <td><?= $x['nip']; ?></td>
-                  <td><?= $x['nama']; ?></td>
-                  <td><?= $x['jenis_kelamin']; ?></td>
-                  <td><?= $x['agama']; ?></td>
-                  <td><img src="../client/foto/<?= $x['foto']; ?>" alt="" width="70px" height="70px"></td>
-                  <td><?= $x['email']; ?></td>
-                  <td><?= $x['username']; ?></td>
-                  <td><?= $x['level']; ?></td>
-                  <td class="text-center">
-                    <button type="button" class="btn btn-success mb-1" data-bs-toggle="modal" data-bs-target="#modalUbah<?= $x['nip']; ?>">
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
-                      </svg> 
-                      Ubah
-                    </button>
+          <table id="table" class="table table-bordered table-hover">
+            <thead class="text-center">
+              <tr>
+                <th class="bg-secondary">No</th>
+                <th class="bg-secondary">NIP</th>
+                <th class="bg-secondary">Nama</th>
+                <th class="bg-secondary">Jenis Kelamin</th>
+                <th class="bg-secondary">Agama</th>
+                <th class="bg-secondary">Foto</th>
+                <th class="bg-secondary">Email</th>
+                <th class="bg-secondary">Username</th>
+                <th class="bg-secondary">Level</th>
+                <th class="bg-secondary">Opsi</th>
+              </tr>
+            </thead>
 
-                    <button type="button" class="btn btn-danger mb-1" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $x['nip']; ?>">
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                      </svg>
-                      Hapus
-                    </button>
-                  </td>
-                </tr>
-              <?php endforeach; ?>
-          </tbody>
-        </table>
+            <tbody>
+              <?php $no = 1; ?>
+                <?php foreach ($data_guru as $x) : ?>
+                  <tr style="height: 15px;">
+                    <td><?= $no++; ?></td>
+                    <td><?= $x['nip']; ?></td>
+                    <td><?= $x['nama']; ?></td>
+                    <td><?= $x['jenis_kelamin']; ?></td>
+                    <td><?= $x['agama']; ?></td>
+                    <td><img src="../client/foto/<?= $x['foto']; ?>" alt="" width="70px" height="70px"></td>
+                    <td><?= $x['email']; ?></td>
+                    <td><?= $x['username']; ?></td>
+                    <td><?= $x['level']; ?></td>
+                    <td class="text-center">
+                      <button type="button" class="btn btn-success mb-1" data-bs-toggle="modal" data-bs-target="#modalUbah<?= $x['nip']; ?>">
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                          <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+                        </svg> 
+                        Ubah
+                      </button>
+
+                      <button type="button" class="btn btn-danger mb-1" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $x['nip']; ?>">
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                        </svg>
+                        Hapus
+                      </button>
+                    </td>
+                  </tr>
+                <?php endforeach; ?>
+            </tbody>
+          </table>
+
+        </div>
+
       </div>
       <!-- /.card-body -->
 
--- ^admin/admin_mapel.php
+++ ^admin/admin_mapel.php
@@ -96,11 +96,11 @@
         <h3 class="card-title"><?= $subtitle; ?></h3>
 
         <form class="form" action="" method="post">
-        <div class="input-group">
-          <input type="text" class="form-control me-3" name="kata_cari" placeholder="Cari..." aria-label="Search" value="<?php if(isset($_POST['cari'])) { echo $_POST['kata_cari']; } ?>">
-          <button class="btn btn-outline-primary me-1" type="submit" name="cari">Cari</button>
-        </div>
-      </form>
+          <div class="input-group">
+            <input type="text" class="form-control me-3" name="kata_cari" placeholder="Cari..." aria-label="Search" value="<?php if(isset($_POST['cari'])) { echo $_POST['kata_cari']; } ?>">
+            <button class="btn btn-outline-primary me-1" type="submit" name="cari">Cari</button>
+          </div>
+        </form>
 
       </div>
       <!-- /.card-header -->
@@ -117,43 +117,48 @@
           Tambah
         </button>  
 
-        <table id="example2" class="table table-bordered table-hover">
-          <thead>
-            <tr>
-              <th class="bg-secondary">No</th>
-              <th class="bg-secondary">Mata Pelajaran</th>
-              <th class="bg-secondary">Opsi</th>
-            </tr>
-          </thead>
+        <div class="table-responsive">
 
-          <tbody>
-            <?php $no = 1; ?>
-              <?php foreach ($data_mapel as $x) : ?>
-                <tr>
-                  <td><?= $no++; ?></td>
-                  <td><?= $x['mpl']; ?></td>
-                  <td class="text-center">
-                    <button type="button" class="btn btn-success mb-1" data-bs-toggle="modal" data-bs-target="#modalUbah<?= $x['id_mapel']; ?>">
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
-                      </svg> 
-                      Ubah
-                    </button>
+          <table id="table" class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th class="bg-secondary">No</th>
+                <th class="bg-secondary">Mata Pelajaran</th>
+                <th class="bg-secondary">Opsi</th>
+              </tr>
+            </thead>
 
-                    <button type="button" class="btn btn-danger mb-1" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $x['id_mapel']; ?>">
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                      </svg>
-                      Hapus
-                    </button>
-                  </td>
-                </tr>
-              <?php endforeach; ?>
-          </tbody>
-        </table>
+            <tbody>
+              <?php $no = 1; ?>
+                <?php foreach ($data_mapel as $x) : ?>
+                  <tr>
+                    <td><?= $no++; ?></td>
+                    <td><?= $x['mpl']; ?></td>
+                    <td class="text-center">
+                      <button type="button" class="btn btn-success mb-1" data-bs-toggle="modal" data-bs-target="#modalUbah<?= $x['id_mapel']; ?>">
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                          <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+                        </svg> 
+                        Ubah
+                      </button>
+
+                      <button type="button" class="btn btn-danger mb-1" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $x['id_mapel']; ?>">
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                        </svg>
+                        Hapus
+                      </button>
+                    </td>
+                  </tr>
+                <?php endforeach; ?>
+            </tbody>
+          </table>
+
+        </div>
+
       </div>
       <!-- /.card-body -->
 
