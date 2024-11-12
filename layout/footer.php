b9f6aafa3be12843633a872431e5faa178bc30a9
Raven Vanzlysky <mandalaraven@outlook.co.id>
2024-11-12 15:10:41 +0700

    merapihkan footer

--- layout/footer.php
+++ layout/footer.php
@@ -1,20 +1,20 @@
-  <footer class="d-flex justify-content-between align-items-center py-3">
+    <footer class="d-flex justify-content-between align-items-center py-3">
 
-    <h6 class="text-start ps-5 animate__animated animate__fadeInLeft">&copy;Raven</h6>
+      <h6 class="text-start ps-5 animate__animated animate__fadeInLeft">&copy;Raven</h6>
 
-    <!-- Switch toggle theme -->
-      <div class="form-check form-switch pe-5 animate__animated animate__fadeInRight">
-        <input class="form-check-input" type="checkbox" id="themeSwitch">
-        <label class="form-check-label" for="themeSwitch">Theme Mode</label>
-      </div>
-    <!-- /Switch toggle theme -->
-      
-  </footer>
-  <script 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous">
-  </script>
+      <!-- Switch toggle theme -->
+        <div class="form-check form-switch pe-5 animate__animated animate__fadeInRight">
+          <input class="form-check-input" type="checkbox" id="themeSwitch">
+          <label class="form-check-label" for="themeSwitch">Theme Mode</label>
+        </div>
+      <!-- /Switch toggle theme -->
+        
+    </footer>
+    <script 
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous">
+    </script>
     <script 
       src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous">
     </script>
